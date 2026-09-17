41c1da8b9e273c15899fa8c1f8465e5120ff705e
KEMILY DE ALMEIDA SILVA <26171663@edu119.local>
2026-09-17 11:23:56 -0300

    subindo

--- servidor.js
+++ servidor.js
@@ -1,18 +1,42 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 let alunos = [
-    {id: 1, nome: "Malu", curso: "Desenvolvimento de Sistemas"},
-    {id: 2, nome: "Camily", curso: "Redes de computadores"},
-    {id: 3, nome: "Kaue", curso: "Banco de dados"},
-    {id: 4, nome: "Rian", curso: "Administração"},
-    {id: 5, nome: "Manu", curso: "Desenvolvimento de Sistemas"},
+    { id: 1, nome: "Malu", curso: "Desenvolvimento de Sistemas" },
+    { id: 2, nome: "Camily", curso: "Redes de computadores" },
+    { id: 3, nome: "Kaue", curso: "Banco de dados" },
+    { id: 4, nome: "Rian", curso: "Administração" },
+    { id: 5, nome: "Manu", curso: "Desenvolvimento de Sistemas" },
 ]
 app.get("/", (req, res) => {
-  res.json({
-    message: "API Alunos funcionando"
-  });
+    res.json({
+        message: "API Alunos funcionando"
+    });
+});
+
+app.get("/alunos", (req, res) => {
+    res.json(alunos);
+});
+
+app.post("/alunos/cadastrar", (req, res) => {
+    //console.log(req.body);
+    const { nome, curso } = req.body;
+    //console.log("nome: " + nome);
+    //console.log(`curso: ${curso}`);
+
+    const id = alunos.length > 0 ? alunos[alunos.length - 1].id + 1 : 1;
+    
+
+    const novoAluno = {
+        nome: nome,
+        curso: curso,
+    }
+
+    alunos.push(novoAluno);
+
+    res.status(201).json({ msg: "Aluno cadastrado com sucesso." });
 });
 
 const PORTA = 3000;
